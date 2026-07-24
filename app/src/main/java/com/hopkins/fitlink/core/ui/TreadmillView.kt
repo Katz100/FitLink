03f3620af2761d173d3d10a3104a4f60520a071c
Katz100 <cody.hopkins108@gmail.com>
2026-07-24 11:07:52 -0700

    Delete treadmillview and just display current machine view in workout screen

--- app/src/main/java/com/hopkins/fitlink/core/ui/TreadmillView.kt
+++ app/src/main/java/com/hopkins/fitlink/core/ui/TreadmillView.kt
@@ -1,31 +1,79 @@
 package com.hopkins.fitlink.core.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hopkins.fitlink.core.data.ConnectionStatus
 import com.hopkins.fitlink.core.ftms.MachineState
+import java.util.Locale
 
 @Composable
 fun TreadmillView(
+    modifier: Modifier = Modifier,
     machineState: MachineState.TreadmillMachine,
+    connectionStatus: ConnectionStatus,
 ) {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+    Box(
+        modifier = modifier.fillMaxSize()
     ) {
         ControlStepper(
-            modifier = Modifier.width(150.dp),
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .width(150.dp),
+            heading = "Inclination",
+            unit = "%",
+            value = if (machineState.inclination == null) {
+                "0.0"
+            } else {
+                machineState.inclination.toString()
+            }
+        )
+
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = connectionStatus.toString(),
+            color = when(connectionStatus) {
+                ConnectionStatus.Connected -> Color.Green
+                is ConnectionStatus.ConnectionError -> Color.Red
+                ConnectionStatus.ConnectionLoading -> Color.Black
+                ConnectionStatus.Disconnected -> Color.Yellow
+            }
+        )
+
+        ControlStepper(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .width(150.dp),
             heading = "Speed",
             unit = "MPH",
-            value = machineState.instantaneousSpeed.toString()
+            value = String.format(Locale.US,"%.1f", machineState.instantaneousSpeed)
+
         )
-        Text(machineState.heartRate.toString())
-        Text(machineState.inclination.toString())
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TreadMillViewPreview() {
+    TreadmillView(
+        modifier = Modifier.size(
+            width = 500.dp,
+            height = 200.dp
+        ),
+        machineState = MachineState.TreadmillMachine(
+            instantaneousSpeed = 500.0,
+            heartRate = 100,
+            inclination = 12.0
+        ),
+        connectionStatus = ConnectionStatus.Connected
+    )
 }
--- app/src/main/java/com/hopkins/fitlink/feature/workout/WorkoutScreen.kt
+++ app/src/main/java/com/hopkins/fitlink/feature/workout/WorkoutScreen.kt
@@ -1,11 +1,13 @@
 package com.hopkins.fitlink.feature.workout
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.hopkins.fitlink.core.ui.WorkoutView
+import com.hopkins.fitlink.core.ftms.MachineState
+import com.hopkins.fitlink.core.ui.TreadmillView
 
 @Composable
 fun WorkoutScreen(
@@ -24,17 +27,20 @@ fun WorkoutScreen(
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.fillMaxSize()
-                .padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            WorkoutView(uiState.machineUiState)
-            Button(
-                onClick = { viewModel.updateSpeed() }
+        when (uiState.machineUiState) {
+            MachineState.DetectingMachine -> Box(
+                modifier = Modifier.fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
             ) {
-                Text("Update speed")
+                CircularProgressIndicator()
+            }
+            is MachineState.TreadmillMachine -> {
+                TreadmillView(
+                    modifier = Modifier.padding(innerPadding),
+                    machineState = uiState.machineUiState,
+                    connectionStatus = uiState.connectionState
+                )
             }
         }
     }
