8b58f3c7cfebdd80a7b8a16f7a66eec40e1d1ae0
Katz100 <cody.hopkins108@gmail.com>
2026-08-16 13:56:10 -0700

    reorder property

--- app/src/main/java/com/hopkins/fitlink/feature/workout/WorkoutScreenViewModel.kt
+++ app/src/main/java/com/hopkins/fitlink/feature/workout/WorkoutScreenViewModel.kt
@@ -38,10 +38,10 @@ class WorkoutScreenViewModel @Inject constructor(
     private val deviceAddress = savedStateHandle.toRoute<Screen.ActiveWorkout>().macAddress
     private var machine: Machine<*>? = null
     private val treadmillSession = TreadmillSessionDomain()
+    val averageHr = mutableListOf<Int>()
 
     private val _workoutUiState = MutableStateFlow<WorkoutUiState>(WorkoutUiState())
     val workoutUiState: StateFlow<WorkoutUiState> = _workoutUiState.asStateFlow()
-    val averageHr = mutableListOf<Int>()
 
     init {
         connectToDevice()
